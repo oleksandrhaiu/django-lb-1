3f94f89b1027f80b8086443ff37f7efcf61bcb10
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-20 23:30:05 +0200

    feat: add shop

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import products
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', products, name='products')
 ]
